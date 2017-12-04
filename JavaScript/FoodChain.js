b860476f7fb7cae0a625dbd0a584b54f5abce2b4
Csizmadiáné Pacsai Judit <judit.pacsai@gmail.com>
2017-12-04 20:28:47 +0100

    adding foodchain.js

---
+++ JavaScript/FoodChain.js
@@ -0,0 +1,27 @@
+// Programming Quiz - Navigating the Food Chain (3-8)
+
+/*
+Write a series of ternary statements that sets the variable category equal to:
+
+"herbivore" if an animal eats plants
+"carnivore" if an animal eats animals
+"omnivore" if an animal eats plants and animals
+undefined if an animal doesn't eat plants or animals
+Use the eatsPlants and eatsAnimals variables to test your code.
+
+*/
+
+//Notes
+//   - use the variables `eatsPlants` and `eatsAnimals` in your ternary expressions
+//   - `if` statements aren't allowed ;-)
+
+// change the values of `eatsPlants` and `eatsAnimals` to test your code
+
+var eatsPlants = false;
+var eatsAnimals = false;
+
+var category = eatsPlants && eatsAnimals ? ("omnivore") : 
+    (eatsPlants ? ("herbivore") : 
+    (eatsAnimals ? ("carnivore") : ("")));
+
+console.log(category);
