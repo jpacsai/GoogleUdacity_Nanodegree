7f4bd2924103ffefa4a58c0c2c0f58a2e2011473
jpacsai <judit.pacsai@gmail.com>
2017-12-13 23:56:15 +0100

    Adding jQuery course work

---
+++ JavaScript/ToggleClass.js
@@ -0,0 +1,7 @@
+/*
+For this quiz, use a jQuery class selector and featuredArticle variable to toggle the 'featured' class!
+*/
+
+var featuredArticle;
+
+featuredArticle = $("li").toggleClass("featured");
