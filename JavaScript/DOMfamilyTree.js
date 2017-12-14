b2ee76b7492aae211ba540f9a854db98eb102904
jpacsai <judit.pacsai@gmail.com>
2017-12-14 02:00:18 +0100

    adding jQuery course work

---
+++ JavaScript/DOMfamilyTree.js
@@ -0,0 +1,18 @@
+/*
+For this quiz, you'll need to add to the DOM tree that already exists.
+
+'#family2' should be a sibling of and come after '#family1'. '#bruce' should be the only immediate child
+of '#family2'. '#bruce' should have two <div>s as children, '#madison' and '#hunter'.
+*/
+
+var family1 = $("#family1");
+var family2 = $("<div id='family2'><h1>Family 2</h1></div>");
+var bruce = $("<div id='bruce'><h2>Bruce</h2></div>");
+var madison = $("<div id='madison'><h3>Madison</h3></div>");
+var hunter = $("<div id='hunter'><h3>Hunter</h3></div>");
+
+
+family2.insertAfter(family1);
+family2.append(bruce);
+bruce.append(madison);
+bruce.append(hunter);
---
+++ JavaScript/RemovingDOMelements.js
@@ -0,0 +1,10 @@
+/*
+For this quiz, remove the <ul> from the first article item!
+
+You must use jQuery's remove() method.
+*/
+
+
+var articleItems;
+
+articleItems = $(".article-item").children("ul").remove();
