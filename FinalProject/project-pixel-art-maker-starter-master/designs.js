4ecb7c9870ae6c9b89bf6eefeb5f70728f3b9395
jpacsai <judit.pacsai@gmail.com>
2017-12-18 00:15:04 +0100

    fixing grid

--- FinalProject/project-pixel-art-maker-starter-master/designs.js
+++ FinalProject/project-pixel-art-maker-starter-master/designs.js
@@ -4,25 +4,26 @@
 // Select size input
 var row;
 var col;
-var t = $("table");
-var r = $("tr");
+var t = $("#pixel_canvas");
 var trow = $("<tr></tr>");
 var tdetail = $("<td></td>");
+
+
 
 // When size is submitted by the user, call makeGrid()
 
 $("input[type=button]").click(function(){
     row = $("#input_height").val();
     col = $("#input_width").val();
-    makeGrid(row); 
+    makeGrid(); 
 });
 
-function makeGrid(row) {
-    for (var rowt = 1; t <= rowt; t++) {
-        for (var tdt = 1; tdt <= col; tdt++) {
-            t.append()
+
+function makeGrid() {
+    for (var x = 1; x <= row; x++) {
+        t.append("<tr></tr>");
+        for (var y = 1; y <= col; y++) {
+            $("tr").append("<td></td>");
         }
     }
-    
-    
 }
