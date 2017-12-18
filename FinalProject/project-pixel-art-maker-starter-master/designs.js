329fb00932e1f492b95954d81563f00c06759c05
jpacsai <judit.pacsai@gmail.com>
2017-12-18 19:45:08 +0100

    fixing paint function

--- FinalProject/project-pixel-art-maker-starter-master/designs.js
+++ FinalProject/project-pixel-art-maker-starter-master/designs.js
@@ -1,37 +1,41 @@
-// Select color input
-
-
-// Select size input
+// Variables
 var row;
 var col;
-var color;
+
 var t = $("#pixel_canvas");
-var trow = $("<tr></tr>");
-var tdetail = $("<td></td>");
-var color = document.getElementById("colorPicker");
-var currentVal = color.value;
+var trow = "<tr></tr>";
+var tdetail = "<td class='canv'></td>";
+
+var color = $("#colorPicker").val();
+
 
 // When size is submitted by the user, call makeGrid()
-
 $("input[type=button]").click(function(){
     row = $("#input_height").val();
     col = $("#input_width").val();
     clearGrid();
     makeGrid(); 
-    alert(currentVal);
 });
-
-
-
-function makeGrid() {
-    for (var x = 1; x <= row; x++) {
-        t.append("<tr></tr>");
-        for (var y = 1; y <= col; y++) {
-            $("tr").last().append("<td></td>");
-        }
-    }
-}
 
 function clearGrid() {
     t.find("tr","td").remove();
 }
+
+function makeGrid() {
+    for (var x = 1; x <= row; x++) {
+        t.append(trow);
+        for (var y = 1; y <= col; y++) {
+            $("tr").last().append(tdetail);
+        }
+    }
+}
+
+// Get color value on change
+$("#colorPicker").change(function() {
+    color = $(this).val();
+});
+
+// Paint function
+$(".canv").mousedown(function() {
+    $( this ).css( "background-color", color );
+  });
