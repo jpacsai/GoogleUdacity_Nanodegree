48cf7d930de09ecbf565c5146810b60ebd087ba0
jpacsai <judit.pacsai@gmail.com>
2017-12-18 14:53:11 +0100

    fixed grid, started color

--- FinalProject/project-pixel-art-maker-starter-master/designs.js
+++ FinalProject/project-pixel-art-maker-starter-master/designs.js
@@ -1,29 +1,37 @@
 // Select color input
-//$(".color").
+
 
 // Select size input
 var row;
 var col;
+var color;
 var t = $("#pixel_canvas");
 var trow = $("<tr></tr>");
 var tdetail = $("<td></td>");
-
-
+var color = document.getElementById("colorPicker");
+var currentVal = color.value;
 
 // When size is submitted by the user, call makeGrid()
 
 $("input[type=button]").click(function(){
     row = $("#input_height").val();
     col = $("#input_width").val();
+    clearGrid();
     makeGrid(); 
+    alert(currentVal);
 });
+
 
 
 function makeGrid() {
     for (var x = 1; x <= row; x++) {
         t.append("<tr></tr>");
         for (var y = 1; y <= col; y++) {
-            $("tr").append("<td></td>");
+            $("tr").last().append("<td></td>");
         }
     }
+}
+
+function clearGrid() {
+    t.find("tr","td").remove();
 }
