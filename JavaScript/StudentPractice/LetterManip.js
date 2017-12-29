759d2668744beff5cc4a2b84f49d5e686496b5fd
jpacsai <judit.pacsai@gmail.com>
2017-12-29 22:20:41 +0100

    adding js practice

---
+++ JavaScript/StudentPractice/LetterManip.js
@@ -0,0 +1,18 @@
+/*
+Write a function that takes a string as an argument and return another string exactly as examples below.
+
+letterManip("abcd");    // "A-Bb-Ccc-Dddd"
+letterManip("HeLlo"); // "H-Ee-Lll-Llll-Ooooo"
+letterManip("chAt");    // "C-Hh-Aaa-Tttt"
+
+*/
+
+function letterManip(letters) {
+    var arr = letters.toLowerCase().split("");
+    var newArr = [];
+      for (var i = 0; i < letters.length; i++) {
+        var letter = arr[i].repeat(i+1);
+        newArr = newArr.concat(letter.charAt(0).toUpperCase() + letter.slice(1));
+      }
+    return newArr.toString().replace(/,/g,"-");
+}
