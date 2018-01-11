12b0d4bc4ddd3fb7bd5a0ef492ca9877e318b734
jpacsai <judit.pacsai@gmail.com>
2018-01-11 23:05:03 +0100

    adding js practice

---
+++ JavaScript/StudentPractice/16_ShiftCapital.js
@@ -0,0 +1,21 @@
+/*
+Shift Capital
+Write a function that takes two arguments, one represents a string that contains a capital letter 
+that can be anywhere in the string , the second argument is a number represent number of 
+shifts of the capital letter in the string when the function is called, if the index that 
+the capital letter should be shifted to is not present , continue shifting from the beginning
+
+For example
+
+shiftCapital(Udacity,4) // daciUty
+shiftCapital(leaRning,4) // Rleaning
+shiftCapital(frienDs,6) // frienDs
+shiftCapital(frienDs,3)  // frDiens
+
+*/
+
+function shiftCapital(str,num) {
+    var newStr = str.replace(str.match(/[A-Z]/),""); 
+    var newIndex = (str.indexOf(str.match(/[A-Z]/))+num)%newStr.length;  
+    return newStr.substr(0,newIndex) + str.match(/[A-Z]/) + newStr.substr(newIndex);   
+}
