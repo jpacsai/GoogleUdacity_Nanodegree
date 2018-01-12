a559679f0ec45ae9a6ebe48d9745ca4d5ce9fa55
Judit Cs. Pacsai <34191321+jpacsai@users.noreply.github.com>
2018-01-12 18:09:21 +0100

    adding js challenge

---
+++ JavaScript/StudentPractice/17_.js
@@ -0,0 +1,15 @@
+function removeCopies(arr) {
+    var i = arr.length;
+    var dupes = [];
+    while (i--) {
+      if (arr.indexOf(arr[i]) != i) {
+                
+      }
+    }
+  console.log(arr);
+    return arr;
+}
+
+
+
+removeCopies([1,6,"a",6,"b",7,8,"a",1]);
