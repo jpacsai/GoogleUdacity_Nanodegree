4b884298ecdb1fbd91a82192eceef4f67fedab37
jpacsai <judit.pacsai@gmail.com>
2018-05-01 22:14:32 +0200

    feat: randomize enemy start position

--- Nanodegree/ArcadeGame/js/app.js
+++ Nanodegree/ArcadeGame/js/app.js
@@ -15,11 +15,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = (this.x + 2 * dt);
+    this.x = (this.x + 3 * dt);
     if (this.x > 7) {
-        this.x = -1;
+        this.x = -1 * (Math.floor(Math.random() * 10) + 3);
+        this.y = Math.floor(Math.random() * (5 - 2) + 2);
     }
-    console.log(this.x);
 };
 
 // Draw the enemy on the screen, required method for game
