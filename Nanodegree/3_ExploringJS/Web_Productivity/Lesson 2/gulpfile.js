0c1a890b3be3bf9952b9a5a200705a0fd83196b2
jpacsai <judit.pacsai@gmail.com>
2018-05-10 21:01:41 +0200

    fix: personalised gulp

--- Nanodegree/3_ExploringJS/Web_Productivity/Lesson 2/gulpfile.js
+++ Nanodegree/3_ExploringJS/Web_Productivity/Lesson 2/gulpfile.js
@@ -2,8 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', function() {
+gulp.task('default', function(done) {
 	console.log('hello world!');
+	done();
 });
 
 gulp.task('styles', function() {
