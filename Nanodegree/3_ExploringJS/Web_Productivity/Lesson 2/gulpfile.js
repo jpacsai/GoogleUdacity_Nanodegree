aaefdb4c2122cb6edb61995881aa5fbc6cdc9fbb
jpacsai <judit.pacsai@gmail.com>
2018-05-10 22:28:04 +0200

    add browser-sync to gulp

--- Nanodegree/3_ExploringJS/Web_Productivity/Lesson 2/gulpfile.js
+++ Nanodegree/3_ExploringJS/Web_Productivity/Lesson 2/gulpfile.js
@@ -1,17 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default', function(done) {
-	console.log('hello world!');
+	gulp.watch('sass/**/*.scss', gulp.series('styles'));
 	done();
 });
 
-gulp.task('styles', function() {
+gulp.task('styles', function(done) {
 	gulp.src('sass/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions']
 		}))
-		.pipe(gulp.dest('./css'));
+		.pipe(gulp.dest('./css'))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
+	done();
 });
+
+browserSync.init({
+    server: "./"
+});
+browserSync.stream();
