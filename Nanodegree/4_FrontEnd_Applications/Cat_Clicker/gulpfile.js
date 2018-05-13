1db45a8372ea2ab4c1d73f03bc46427bb12b3849
jpacsai <judit.pacsai@gmail.com>
2018-05-13 21:56:56 +0200

    feat: set up gulp sass

--- Nanodegree/4_FrontEnd_Applications/Cat_Clicker/gulpfile.js
+++ Nanodegree/4_FrontEnd_Applications/Cat_Clicker/gulpfile.js
@@ -1,28 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
-var babel = require('gulp-babel');
 
-gulp.task('default', function(done) {
-	gulp.watch('sass/**/*.scss', gulp.series('styles'));
-	done();
+gulp.task('default', function() {
+	gulp.watch('sass/**/*.scss', ['styles']);
+	
 });
 
-gulp.task('styles', function(done) {
+gulp.task('styles', function() {
 	gulp.src('sass/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions']
 		}))
-		.pipe(gulp.dest('./css'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
-	done();
+		.pipe(gulp.dest('./css'));
+	
 });
-
-browserSync.init({
-	server: './'
-});
-browserSync.stream();
