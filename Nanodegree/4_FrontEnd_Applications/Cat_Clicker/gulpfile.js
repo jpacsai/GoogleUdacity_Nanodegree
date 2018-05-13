56e2bbf3001d1200611c55f0ffa5a213bf19a9ad
jpacsai <judit.pacsai@gmail.com>
2018-05-13 23:45:06 +0200

    feat: setup gulp copy-function

--- Nanodegree/4_FrontEnd_Applications/Cat_Clicker/gulpfile.js
+++ Nanodegree/4_FrontEnd_Applications/Cat_Clicker/gulpfile.js
@@ -3,9 +3,10 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 
-gulp.task('default', ['browser-sync'], function() {
+gulp.task('default', ['styles', 'copy-img', 'copy-js', 'copy-html', 'browser-sync'], function() {
 	gulp.watch('sass/**/*.scss', ['styles']);
 	gulp.watch("images/*", ['copy-img']);
+	gulp.watch("js/*", ['copy-js']);
 	gulp.watch("./*.html", ['copy-html', browserSync.reload]);
 });
 
@@ -26,7 +27,12 @@ gulp.task('copy-html', function() {
 
 gulp.task('copy-img', function() {
 	gulp.src('./images/*')
-		.pipe(gulp.dest('dist/images'))
+		.pipe(gulp.dest('dist/images'));
+})
+
+gulp.task('copy-js', function() {
+	gulp.src('./js/*')
+		.pipe(gulp.dest('dist/js'));
 })
 
 gulp.task('browser-sync', function() {
