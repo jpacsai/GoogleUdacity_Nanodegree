4e406d8c6393c4bc53828ecd0361cc850f5069df
jpacsai <judit.pacsai@gmail.com>
2018-05-14 22:29:50 +0200

    feat: OOP multiple cat click counter

--- Nanodegree/4_FrontEnd_Applications/Cat_Clicker/dist/js/app.js
+++ Nanodegree/4_FrontEnd_Applications/Cat_Clicker/dist/js/app.js
@@ -1,12 +1,76 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 document.addEventListener("DOMContentLoaded", function () {
-    var clickCount = 0;
-    var cat = document.querySelector('.cat');
-    var countDisplay = document.querySelector('.click-count');
-    cat.onclick = function () {
-        clickCount++;
-        console.log(clickCount);
-        countDisplay.textContent = clickCount;
+    var Cat = function () {
+        function Cat(id, name, url) {
+            _classCallCheck(this, Cat);
+
+            this.id = id;
+            this.name = name;
+            this.url = url;
+            this.click = 0;
+            this.create();
+        }
+
+        _createClass(Cat, [{
+            key: 'create',
+            value: function create() {
+                var kittyContainer = document.createElement('SECTION');
+                kittyContainer.id = this.id;
+                kittyContainer.classList.add('kittyContainer', this.name);
+
+                var kittyHeader = document.createElement('H3');
+                kittyHeader.classList.add('instruction');
+                kittyHeader.textContent = 'Click on ' + this.name + '!';
+
+                var catImage = document.createElement('IMG');
+                catImage.classList.add('cat');
+                catImage.src = this.url;
+
+                var counter = document.createElement('DIV');
+                counter.classList.add('counter');
+                counter.textContent = 'Clicks: ';
+
+                var clickCounter = document.createElement('SPAN');
+                clickCounter.classList.add('click-count');
+                clickCounter.textContent = this.click;
+
+                counter.appendChild(clickCounter);
+
+                kittyContainer.append(kittyHeader, catImage, counter);
+
+                var app = document.querySelector('.container');
+                app.appendChild(kittyContainer);
+            }
+        }, {
+            key: 'clicker',
+            value: function clicker() {
+                this.click++;
+            }
+        }]);
+
+        return Cat;
+    }();
+
+    var cecile = new Cat('cecile', 'Cecile', 'images/cat.jpg');
+    var bertalan = new Cat('bertalan', 'Bertalan', 'images/cat.jpg');
+
+    var cats = Array.from(document.getElementsByClassName('cat'));
+
+    for (var c in cats) {
+        cats[c].onclick = function (event) {
+            catClicked(event);
+        };
+    }
+
+    function catClicked(event) {
+        var element = event.target.parentElement;
+        var obj = eval(element.id);
+        obj.clicker();
+        element.lastChild.lastChild.textContent = obj.click;
     };
 });
--- Nanodegree/4_FrontEnd_Applications/Cat_Clicker/js/app.js
+++ Nanodegree/4_FrontEnd_Applications/Cat_Clicker/js/app.js
@@ -1,10 +1,64 @@
 document.addEventListener("DOMContentLoaded", function(){
-    let clickCount = 0;
-    const cat = document.querySelector('.cat');
-    const countDisplay = document.querySelector('.click-count');
-    cat.onclick = function(){
-        clickCount++;
-        console.log(clickCount);
-        countDisplay.textContent = clickCount;
+    class Cat {
+        constructor (id, name, url) {
+            this.id = id;
+            this.name = name;
+            this.url = url;
+            this.click = 0;
+            this.create();
+        }
+
+        create() {
+            let kittyContainer = document.createElement('SECTION');
+            kittyContainer.id = this.id;
+            kittyContainer.classList.add('kittyContainer', this.name);
+
+            let kittyHeader = document.createElement('H3');
+            kittyHeader.classList.add('instruction');
+            kittyHeader.textContent = 'Click on ' + this.name + '!';
+
+            let catImage = document.createElement('IMG');
+            catImage.classList.add('cat');
+            catImage.src = this.url;
+
+            let counter = document.createElement('DIV');
+            counter.classList.add('counter');
+            counter.textContent = 'Clicks: ';
+
+            let clickCounter = document.createElement('SPAN');
+            clickCounter.classList.add('click-count');
+            clickCounter.textContent = this.click;
+
+            counter.appendChild(clickCounter);
+
+            kittyContainer.append(kittyHeader, catImage, counter);
+
+            const app = document.querySelector('.container');
+            app.appendChild(kittyContainer);
+        }
+
+        clicker() {
+            this.click++;
+        }
+    }
+
+    let cecile = new Cat('cecile', 'Cecile', 'images/cat.jpg');
+    let bertalan = new Cat('bertalan', 'Bertalan', 'images/cat.jpg');
+
+    let cats = Array.from(document.getElementsByClassName('cat'));
+
+    for (let c in cats) {
+        cats[c].onclick = function(event) {
+            catClicked(event);
+        };
+    }
+
+    function catClicked(event){
+        let element = event.target.parentElement;
+        let obj = eval(element.id);
+        obj.clicker();
+        element.lastChild.lastChild.textContent = obj.click;
+        
     };
+
 });
