d2791bf9cebb43a19c98e05060e48e5bdfe74c63
jpacsai <judit.pacsai@gmail.com>
2018-06-27 15:59:37 +0200

    feat: add alt text to images

--- Nanodegree/Restaurant_Review_App/js/main.js
+++ Nanodegree/Restaurant_Review_App/js/main.js
@@ -142,6 +142,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = 'image of ' + restaurant.name + ' restaurant';
   li.append(image);
 
   const name = document.createElement('h1');
