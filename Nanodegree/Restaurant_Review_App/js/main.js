3319015cbd6d48c679109874a37f073a2e9a6f55
jpacsai <judit.pacsai@gmail.com>
2018-06-29 00:22:32 +0200

    feat: add service worker

--- Nanodegree/Restaurant_Review_App/js/main.js
+++ Nanodegree/Restaurant_Review_App/js/main.js
@@ -1,3 +1,16 @@
+
+if('serviceWorker' in navigator) {
+  console.log('CLIENT: service worker registration in progress.');
+  navigator.serviceWorker.register('sw.js').then(function() {
+    console.log('CLIENT: service worker registration complete.');
+  }, function() {
+    console.log('CLIENT: service worker registration failure.');
+  });
+} else {
+  console.log('CLIENT: service worker is not supported.');
+}
+
+
 let restaurants,
   neighborhoods,
   cuisines
