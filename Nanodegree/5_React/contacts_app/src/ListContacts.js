e265e4182d79b4bffc37bea468283e9f67c3bfbb
jpacsai <judit.pacsai@gmail.com>
2018-07-03 14:09:41 +0200

    feat: search contacts

--- Nanodegree/5_React/contacts_app/src/ListContacts.js
+++ Nanodegree/5_React/contacts_app/src/ListContacts.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class ListContacts extends Component {
   static propTypes = {
@@ -12,14 +14,21 @@ class ListContacts extends Component {
   }
 
   updateQuery = (query) => {
-    console.log('change!');
     this.setState({ query: query.trim() })
   }
 
   render() {
+    let showingContacts;
+    if (this.state.query) {
+      const match = new RegExp(escapeRegExp(this.state.query), 'i');
+      showingContacts = this.props.contacts.filter((c) => match.test(c.name));
+    }
+    else {
+      showingContacts = this.props.contacts;
+    }
+
     return (
       <div className='list-contacts'>
-        {JSON.stringify(this.state)}
         <div className='list-contacts-top'>
           <input 
             className='search-contacts'
@@ -30,7 +39,7 @@ class ListContacts extends Component {
           />
         </div>
         <ol className='contact-list'>
-          {this.props.contacts.map((contact) => (
+          {showingContacts.map((contact) => (
             <li key={contact.id} className='contact-list-item'>
               <div className='contact-avatar' style={{
                 backgroundImage: `url(${contact.avatarURL})`
