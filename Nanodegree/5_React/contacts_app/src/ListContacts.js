5ce92db3c9a3ddc59ba7c0986249b15c30460863
jpacsai <judit.pacsai@gmail.com>
2018-07-03 12:55:26 +0200

    refactor: function to class

--- Nanodegree/5_React/contacts_app/src/ListContacts.js
+++ Nanodegree/5_React/contacts_app/src/ListContacts.js
@@ -1,30 +1,33 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
-function ListContacts (props) {
-  return (
-    <ol className='contact-list'>
-      {props.contacts.map((contact) => (
-        <li key={contact.id} className='contact-list-item'>
-          <div className='contact-avatar' style={{
-            backgroundImage: `url(${contact.avatarURL})`
-          }}/>
-          <div className='contact-details'>
-            <p>{contact.name}</p>
-            <p>{contact.email}</p>
-          </div>
-          <button onClick={() => props.onDeleteContact(contact)} className='contact-remove'>
-            Remove
-          </button>
-        </li>
-      ))}
-    </ol>
-  )
-}
+class ListContacts extends Component {
 
-ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
+
+  render() {
+    return (
+      <ol className='contact-list'>
+        {this.props.contacts.map((contact) => (
+          <li key={contact.id} className='contact-list-item'>
+            <div className='contact-avatar' style={{
+              backgroundImage: `url(${contact.avatarURL})`
+            }}/>
+            <div className='contact-details'>
+              <p>{contact.name}</p>
+              <p>{contact.email}</p>
+            </div>
+            <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+              Remove
+            </button>
+          </li>
+        ))}
+      </ol>
+    )
+  }
 }
 
 export default ListContacts
