cec7a214fb504a9f6cdfad62d3336bf2c0943926
jpacsai <judit.pacsai@gmail.com>
2018-07-03 14:24:03 +0200

    style: clean up variables with destructuring

--- Nanodegree/5_React/contacts_app/src/ListContacts.js
+++ Nanodegree/5_React/contacts_app/src/ListContacts.js
@@ -18,13 +18,17 @@ class ListContacts extends Component {
   }
 
   render() {
+    const { contacts, onDeleteContact } = this.props;
+    const { query } = this.state;
+
+
     let showingContacts;
-    if (this.state.query) {
-      const match = new RegExp(escapeRegExp(this.state.query), 'i');
-      showingContacts = this.props.contacts.filter((c) => match.test(c.name));
+    if (query) {
+      const match = new RegExp(escapeRegExp(query), 'i');
+      showingContacts = contacts.filter((c) => match.test(c.name));
     }
     else {
-      showingContacts = this.props.contacts;
+      showingContacts = contacts;
     }
 
     showingContacts.sort(sortBy('name'));
@@ -36,8 +40,8 @@ class ListContacts extends Component {
             className='search-contacts'
             type='text'
             placeholder='Search contacts'
-            value={this.state.query}
-            onChange={(event) => this.updateQuery(event.target.value)}
+            value={ query }
+            onChange={ (event) => this.updateQuery(event.target.value) }
           />
         </div>
         <ol className='contact-list'>
