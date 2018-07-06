ccbfaabff02aa39878e7c51a28a5e4de7b0ad30e
jpacsai <judit.pacsai@gmail.com>
2018-07-07 01:23:24 +0200

    feat: submit new contact to server

--- Nanodegree/5_React/contacts_app/src/App.js
+++ Nanodegree/5_React/contacts_app/src/App.js
@@ -24,6 +24,16 @@ class App extends Component {
     ContactsAPI.remove(contact)
   }
 
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => (
+        {
+          contacts: state.contacts.concat([ contact ])
+        }
+      ))
+    })
+  }
+
   render() {
     return (
       <div>
@@ -33,7 +43,14 @@ class App extends Component {
           onDeleteContact={this.removeContact}
           />
         )}/>
-        <Route path='/create' component={ CreateContact }/>
+        <Route path='/create' render={ ({ history }) => 
+          <CreateContact 
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+
+          /> }/>
       </div>
     )
   }
