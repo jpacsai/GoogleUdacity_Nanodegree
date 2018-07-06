2591ebce38336fa071eba7a4afceed7143f03a43
jpacsai <judit.pacsai@gmail.com>
2018-07-07 01:00:25 +0200

    feat: add contact form

--- Nanodegree/5_React/contacts_app/src/CreateContacts.js
+++ Nanodegree/5_React/contacts_app/src/CreateContacts.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import ImageInput from './ImageInput';
 
 class CreateContact extends Component {
     render() {
         return (
-            <div>Create contact!</div>
+            <div>
+                <Link className='close-create-contact' to='/'>Close</Link>
+                <form className='create-contact-form'>
+                    <ImageInput 
+                        className='create-contact-avatar-input'
+                        name='avatarURL'
+                        maxHeight={64}
+                    />
+                    <div className='create-contact-details'>
+                        <input type='text' name='name' placeholder='Name'/>
+                        <input type='text' name='email' placeholder='Email'/>
+                        <button>Add contact</button>
+                    </div>
+                </form>
+            </div>
         )
     }
 }
