b5bf00dfd513a85edb70389a652718851e791c7a
jpacsai <judit.pacsai@gmail.com>
2018-07-08 00:57:31 +0200

    style: move bookshelves into seperate component

--- Nanodegree/MyReads_App/src/ListBooks.js
+++ Nanodegree/MyReads_App/src/ListBooks.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Component } from 'react'
 import { Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
+import BookShelf from './BookOnShelf';
 
 class ListBooks extends Component {
     state = {
@@ -33,86 +34,17 @@ class ListBooks extends Component {
               <div>
                 <div className="bookshelf">
                     <h2 className="bookshelf-title">Currently Reading</h2>
-                    <div className="bookshelf-books">
-                      <ol className="books-grid">
-                        { this.state.currentlyReading.map((book) => (
-                          <li key={'book.industryIdentifiers[0].identifier'}>
-                            <div className="book">
-                              <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("'+book.imageLinks.smallThumbnail+'")' }}></div>
-                                <div className="book-shelf-changer">
-                                  <select>
-                                    <option value="move" disabled>Move to...</option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead">Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                  </select>
-                                </div>
-                              </div>
-                              <div className="book-title">{book.title}</div>
-                              <div className="book-authors">{book.authors}</div>
-                            </div>
-                          </li>
-                        ))}
-                      </ol>
-                    </div>
+                    <BookShelf shelf={this.state.currentlyReading} />
                 </div>
 
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      { this.state.wantToRead.map((book) => (
-                        <li key={'book.industryIdentifiers[0].identifier'}>
-                          <div className="book">
-                            <div className="book-top">
-                              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("'+book.imageLinks.smallThumbnail+'")' }}></div>
-                              <div className="book-shelf-changer">
-                                <select>
-                                  <option value="move" disabled>Move to...</option>
-                                  <option value="currentlyReading">Currently Reading</option>
-                                  <option value="wantToRead">Want to Read</option>
-                                  <option value="read">Read</option>
-                                  <option value="none">None</option>
-                                </select>
-                              </div>
-                            </div>
-                            <div className="book-title">{book.title}</div>
-                            <div className="book-authors">{book.authors}</div>
-                          </div>
-                        </li>
-                      ))} 
-                    </ol>
-                  </div>
+                  <BookShelf shelf={this.state.wantToRead} />
                 </div>
 
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      { this.state.read.map((book) => (
-                        <li key={'book.industryIdentifiers[0].identifier'}>
-                          <div className="book">
-                            <div className="book-top">
-                              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("'+book.imageLinks.smallThumbnail+'")' }}></div>
-                              <div className="book-shelf-changer">
-                                <select>
-                                  <option value="move" disabled>Move to...</option>
-                                  <option value="currentlyReading">Currently Reading</option>
-                                  <option value="wantToRead">Want to Read</option>
-                                  <option value="read">Read</option>
-                                  <option value="none">None</option>
-                                </select>
-                              </div>
-                            </div>
-                            <div className="book-title">{book.title}</div>
-                            <div className="book-authors">{book.authors}</div>
-                          </div>
-                        </li>
-                      ))}
-                    </ol>
-                  </div>
+                  <BookShelf shelf={this.state.read} />
                 </div>
               </div>
             </div>
