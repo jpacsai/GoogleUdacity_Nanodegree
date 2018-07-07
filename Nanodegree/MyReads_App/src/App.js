331984ed7774a02f5ee4b802658c9fb23b267b8b
jpacsai <judit.pacsai@gmail.com>
2018-07-07 13:18:05 +0200

    feat: add basic routing

--- Nanodegree/MyReads_App/src/App.js
+++ Nanodegree/MyReads_App/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import { Route } from 'react-router-dom'
+import SearchBooks from './SearchBooks';
 
 class BooksApp extends React.Component {
   state = {
@@ -16,28 +18,8 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
+        
+        <Route exact path='/' render= { () => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -198,6 +180,10 @@ class BooksApp extends React.Component {
             </div>
           </div>
         )}
+        />
+
+        <Route path='/search' component={ SearchBooks } />          
+
       </div>
     )
   }
